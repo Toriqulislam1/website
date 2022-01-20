5c281d9768bdcff4c667eff9b30dc99ef59e136a
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2022-01-20 14:32:22 +0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<form>
+       <h2> Application Form for Review of Answer Papers (Held-December 2019-January 2020)</h2>
+   
+
+   <label for=" Name Of Examiner"> 1. Name Of Examiner (Capital Letter In English:) *</label><br>
+   <input type="text" name="Examiner"><br>
+   
+
+   <label for=" Name Of Examiner"> 2. Technology: *</label><br>
+    <select> 
+       <option>select Technology</option>
+       <option>computer Technology</option>
+       <option>civil Technology</option>
+       <option>Electrical Technology</option>
+       <option>power Technology</option>
+       <option>mechanical Technology</option>
+       <option>Elecromedical Technology</option>
+       <option>Electronics Technology</option>
+      
+    </select><br>
+
+   <label for=" Name Of Examiner">3. Episode: *</label><br>
+   <input type="text" name="Examiner" ><br>
+
+   <label for=" Name Of Examiner">4. Board Roll</label><br>
+   <input type="number" name="Examiner" placeholder="Roll No"><br>
+
+   <label for=" Name Of Examiner"> 5. Reg: Number: *</label><br>
+   <input type="number" name="Examiner" placeholder="Reg No"><br>
+
+   <label for=" Name Of Examiner"> 6.. Observed Subject Code: *</label><br>
+   <input type="number" name="Examiner" placeholder="Example:111,112"><br>
+
+   <label for=" Name Of Examiner"> 7.Total Subjects: *</label><br>
+   <input type="number" name="Examiner" placeholder="Total Subject"><br>
+
+
+   <label for=" Name Of Examiner">8.Total Fee / Amount (At The Rate Of 300 / - Per Subject) *</label><br>
+   <input type="number" name="Examiner" placeholder="Fee"><br>
+
+   <label for=" Name Of Examiner">9. DD Fee: 50 / - (Per Student) *</label><br>
+   <input type="number" name="Examiner" placeholder="DD Fee"><br>
+
+   <label for=" Name Of Examiner">10. SureCash TXN ID NO: *</label><br>
+   <input type="number" name="Examiner" placeholder="Sure Cash"><br>
+
+   <label for=" Name Of Examiner"> 11. Observed Subject Code: *</label><br>
+   <input type="text" name="Examiner">
+
+   <label for=" submit"> </label><br>
+   <input type="submit"  value="submit"><br>
+
+
+
+ <p > Any problem hotline - 01742-62958 / 019999432618
+   SureCash hotline - 01970001525</p>
+
+
+
+
+
+ </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
